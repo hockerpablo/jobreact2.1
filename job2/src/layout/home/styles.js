eafd09f646915d778cf37aad1ce46de0a251a2ba
hockerpablo <hockeralma22@gmail.com>
2023-11-27 20:06:45 -0300

    fist commit

---
+++ job2/src/layout/home/styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+
+export const LayoutContainerStyled  = styled.div `
+background-image: url('../../../public/background2.png');
+background-repeat: no-repeat;
+background-attachment: fixed;
+
+`
+
+export const ContentContainerStyled = styled.div`
+background-color: transparent;
+
+`
